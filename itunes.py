053f7cdd4e240ca4a3de25512fc0d5d8f9d2afe8
Hikari YAMAGUCHI <103912221+youkouyamagechi@users.noreply.github.com>
2022-06-27 03:05:58 +0000

    変えた

--- itunes.py
+++ itunes.py
@@ -17,3 +17,8 @@ print(json.dumps(data, indent=4, ensure_ascii=False).encode().decode())
 
 # 件数の表示
 print('検索結果：' + str(data['resultCount']) + ' 件')
+
+print(date['results'][0]['collectionName'])
+print(date['results'][0] ['trackName'] 
+
+      
